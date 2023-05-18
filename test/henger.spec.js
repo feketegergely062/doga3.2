eb9997dd97375bbe244c0b9395660cc2967ac80e
Fekete Gergely <feketegergely062@gmail.com>
2023-05-18 15:17:27 +0200

    test

--- test/henger.spec.js
+++ test/henger.spec.js
@@ -3,4 +3,17 @@ describe('henger test', ()=>{
         let act = calcSurface(1,200);
         expect(act).toBeCloseTo(1262.9)
     });
+    it('8,400', () => {
+        let act = calcSurface(8,400);
+        expect(act).toBeCloseTo(20508.3)
+    });
+    it('9,500', () => {
+        let act = calcSurface(9,500);
+        expect(act).toBeCloseTo(28783.27)
+    });
+    it('9,-500', () => {
+        let act = calcSurface(9,-500);
+        expect(act).toBeCloseTo("hibás beviteli érték!")
+    });
+
 })
