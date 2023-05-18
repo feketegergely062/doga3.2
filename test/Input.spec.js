4c07b07aa45e18f10de671b89c403a567a5f3465
Fekete Gergely <feketegergely062@gmail.com>
2023-05-18 15:01:23 +0200

    test

--- test/Input.spec.js
+++ test/Input.spec.js
@@ -1,1 +1,15 @@
-describe
+describe('bevitel teszt, szám és betű', () => {
+    it('8', () => {
+        let act = isGoodInput(8)
+        expect(true).toBe(act);
+    });
+    it('xy', () => {
+        let act = isGoodInput(xy)
+        expect(false).toBe(act);
+    });
+    it('8x', () => {
+        let act = isGoodInput("8x")
+        expect(false).toBe(act);
+    });
+
+})
